ea9f9fd89a0a5dc5a83a50d17bc18ffe36566fe8
Anton Malkov <malkoff77@yandex.ru>
2023-11-21 12:49:24 +0300

    changed the code

---
+++ src/test/java/ru/netology/page/PaymentGate.java
@@ -0,0 +1,39 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.SelenideElement;
+import ru.netology.data.DataHelper;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class PaymentGate {
+
+    // инициализировал элементы на странице
+    private final SelenideElement numberCardField = $(byText("Номер карты")).parent().$("input");
+    private final SelenideElement monthField = $(byText("Месяц")).parent().$("input");
+    private final SelenideElement yearField = $(byText("Год")).parent().$("input");
+    private final SelenideElement holderCardField = $(byText("Владелец")).parent().$("input");
+    private final SelenideElement codeField = $(byText("CVC/CVV")).parent().$("input");
+    private final SelenideElement loginButton = $(byText("Продолжить"));
+    private final SelenideElement buy = $(byText("Купить"));
+
+    public void validCard() { // метод заполнения валидных данных
+        buy.click();          // инициализировал поля и заполнил данными
+        numberCardField.setValue(DataHelper.generateValidRandomNumberCard());
+        monthField.setValue(DataHelper.generateRandomMonth());
+        yearField.setValue(String.valueOf(DataHelper.generateRandomYear()));
+        holderCardField.setValue(DataHelper.generateRandomNameHolderCard());
+        codeField.setValue(DataHelper.generatorRandomCode());
+        loginButton.click();
+    }
+    public PaymentGate invalidCard(DataHelper.AuthInfo info) { // тут будет метод невалидных данных
+        buy.click();                                           // Пробовал реализовать метод по-другому
+        numberCardField.setValue(info.getNumberCard());        //
+        monthField.setValue((info.getMonth()));
+        yearField.setValue(String.valueOf(info.getYear()));
+        holderCardField.setValue(info.getHolderCard());
+        codeField.setValue(info.getCode());
+        loginButton.click();
+        return new PaymentGate();
+    }
+}
--- src/test/java/ru/netology/test/PaymentGateTest.java
+++ src/test/java/ru/netology/test/PaymentGateTest.java
@@ -2,7 +2,9 @@ package ru.netology.test;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import ru.netology.data.DataHelper;
+import ru.netology.page.PaymentGate;
 
 import java.time.Duration;
 
@@ -11,7 +13,10 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 
+
 public class PaymentGateTest {
+
+    PaymentGate paymentGate; //Инициализировал страницу PaymentGate
 
     @BeforeEach
     void setup() {
@@ -20,32 +25,27 @@ public class PaymentGateTest {
 
     @Test
     void validDataTest() {
-        $(byText("Купить")).click();
-        $(byText("Номер карты")).parent().$("input").setValue(String.valueOf(DataHelper.generateValidRandomNumberCard()));
-        $(byText("Месяц")).parent().$("input").setValue(String.valueOf(DataHelper.generateRandomMonth()));
-        $(byText("Год")).parent().$("input").setValue(String.valueOf(DataHelper.generateRandomYear()));
-        $(byText("Владелец")).parent().$("input").setValue(DataHelper.generateRandomNameHolderCard());
-        $(byText("CVC/CVV")).parent().$("input").setValue(String.valueOf(DataHelper.generatorRandomCode()));
-        $(byText("Продолжить")).click();
+        //var authinfo = DataHelper.generateRandomCard();
+        paymentGate.validCard();   //прошу выполнить метод validCard, который на странице PaymentGate
         $(byText("Успешно")).shouldBe(visible, Duration.ofSeconds(10));
     }
-    @Test
-    void invalidDataTest() {
-        $(byText("Купить")).click();
-        $(byText("Номер карты")).parent().$("input").setValue(String.valueOf(DataHelper.invalidNumberCard()));
-        $(byText("Месяц")).parent().$("input").setValue(String.valueOf(DataHelper.generateRandomMonth()));
-        $(byText("Год")).parent().$("input").setValue(String.valueOf(DataHelper.generateRandomYear()));
-        $(byText("Владелец")).parent().$("input").setValue(DataHelper.generateRandomNameHolderCard());
-        $(byText("CVC/CVV")).parent().$("input").setValue(String.valueOf(DataHelper.generatorRandomCode()));
-        $(byText("Продолжить")).click();
-        $(byText("Неверный формат")).shouldBe(visible, Duration.ofSeconds(10));
-    }
-    @Test
-    void emptyFieldsTest() {
-        $(byText("Купить")).click();
-        $(byText("Продолжить")).click();
-        $(byText("Неверный формат")).shouldBe(visible, Duration.ofSeconds(10));
-    }
+    //@Test
+   // void invalidDataTest() {
+       // $(byText("Купить")).click();
+      //  $(byText("Номер карты")).parent().$("input").setValue(String.valueOf(DataHelper.invalidNumberCard()));
+       // $(byText("Месяц")).parent().$("input").setValue(String.valueOf(DataHelper.generateRandomMonth()));
+       // $(byText("Год")).parent().$("input").setValue(String.valueOf(DataHelper.generateRandomYear()));
+      //  $(byText("Владелец")).parent().$("input").setValue(DataHelper.generateRandomNameHolderCard());
+       // $(byText("CVC/CVV")).parent().$("input").setValue(String.valueOf(DataHelper.generatorRandomCode()));
+       // $(byText("Продолжить")).click();
+       // $(byText("Неверный формат")).shouldBe(visible, Duration.ofSeconds(10));
+   // }
+    //@Test
+    //void emptyFieldsTest() {
+       // $(byText("Купить")).click();
+       // $(byText("Продолжить")).click();
+       // $(byText("Неверный формат")).shouldBe(visible, Duration.ofSeconds(10));
+    //}
 
 
 }
